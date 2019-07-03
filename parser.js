91993d2be53bd016987a2a5c5d910faeb009d00f
Martin Jordanov <xxm0703@abv.bg>
2019-07-03 14:48:05 +0300

    Bug fix

--- parser.js
+++ parser.js
@@ -12,9 +12,17 @@ class JSONParser {
 
     import(string) {
         let object = JSON.parse(string);
+        let map = new Array();
+        let temp_aisle, segments;
 
-        for (const aisle of object.aisle) {
-            
+        for (const aisle of object.aisles) {
+            temp_aisle = new Aisle(aisle.start, aisle.end)
+            segments = aisle.segments.map((e) => new Segment(e.start, e.end).attach(temp_aisle)); 
+            temp_aisle.segments = segments;
+
+            map.push(temp_aisle);
         }
+        
+        return map;
     }
 }
